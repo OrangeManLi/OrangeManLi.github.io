5d1182407644c545b6217df0080fabd841aa32a0
orange <licheng@xueqiu.com>
2019-10-14 19:20:58 +0800

    test: 1

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>开发并且测试可以吗！！！！</p>
+        <p>测试开发开发并且测试可以吗！！！！</p>
         <a
           className="App-link"
           href="https://reactjs.org"
