4ac732cd066231ab52465ac38a0d6eee416e3f12
OrangeManLi <OrangeManLi@163.com>
2020-08-11 15:42:21 +0000

    Deploying to gh-pages from master dd5d4b052f50183e2745212145f685855cb8be80

---
+++ precache-manifest.544b807a779ba29fdfca86130fcc944d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1aece661ce48798b1397c18de420093a",
+    "url": "/demo/index.html"
+  },
+  {
+    "revision": "055419f192cce745a2b3",
+    "url": "/demo/static/css/main.b4c20afa.chunk.css"
+  },
+  {
+    "revision": "1fe86b1665564c5f0e88",
+    "url": "/demo/static/js/2.a5a2b4ae.chunk.js"
+  },
+  {
+    "revision": "055419f192cce745a2b3",
+    "url": "/demo/static/js/main.13a94ec0.chunk.js"
+  },
+  {
+    "revision": "bdaa386fdb348152ecab",
+    "url": "/demo/static/js/runtime-main.fb5be54f.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/demo/static/media/logo.25bf045c.svg"
+  }
+]);
